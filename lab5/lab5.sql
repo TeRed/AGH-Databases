451d08e045bec98fb33c6444291946d265c0a2df
Albert Duz <dddd.albert@gmail.com>
2018-11-18 17:28:03 +0100

    lab5 homework init

--- lab5/lab5.sql
+++ lab5/lab5.sql
@@ -16,6 +16,27 @@ GROUP BY p.idpudelka
 ORDER BY 2 DESC
 LIMIT 1;
 
+SELECT p.idpudelka, SUM(z.sztuk)
+FROM pudelka p JOIN zawartosc z USING(idpudelka)
+GROUP BY p.idpudelka
+ORDER BY 2 DESC;
+
+SELECT p.idpudelka, SUM(z.sztuk)
+FROM    pudelka p
+        JOIN zawartosc z USING(idpudelka)
+        JOIN czekoladki c USING(idczekoladki)
+WHERE c.orzechy IS NULL
+GROUP BY p.idpudelka
+ORDER BY 2 DESC;
+
+SELECT p.idpudelka, SUM(z.sztuk)
+FROM    pudelka p
+        JOIN zawartosc z USING(idpudelka)
+        JOIN czekoladki c USING(idczekoladki)
+WHERE c.czekolada = 'mleczna'
+GROUP BY p.idpudelka
+ORDER BY 2 DESC;
+
 -- 5.2
 
 SELECT p.idpudelka, SUM(c.masa * z.sztuk)
@@ -33,6 +54,18 @@ GROUP BY p.idpudelka
 ORDER BY 2 DESC
 LIMIT 1;
 
+SELECT SUM(c.masa * z.sztuk) / COUNT(DISTINCT p.idpudelka)
+FROM    pudelka p
+        JOIN zawartosc z USING(idpudelka)
+        JOIN czekoladki c USING(idczekoladki);
+
+SELECT p.idpudelka, SUM(c.masa * z.sztuk) / SUM(z.sztuk)
+FROM    pudelka p
+        JOIN zawartosc z USING(idpudelka)
+        JOIN czekoladki c USING(idczekoladki)
+GROUP BY p.idpudelka
+ORDER BY 2 DESC;
+
 -- 5.3
 
 SELECT datarealizacji, COUNT(datarealizacji)
@@ -42,6 +75,18 @@ ORDER BY 2 DESC;
 
 SELECT COUNT(*)
 FROM zamowienia;
+
+SELECT SUM(a.sztuk * p.cena)
+FROM    zamowienia z
+        JOIN artykuly a USING(idzamowienia)
+        JOIN pudelka p USING(idpudelka);
+
+SELECT z.idklienta, COUNT(idzamowienia), SUM(a.sztuk * p.cena)
+FROM    zamowienia z
+        JOIN artykuly a USING(idzamowienia)
+        JOIN pudelka p USING(idpudelka)
+GROUP BY z.idklienta
+ORDER BY 1;
 
 -- 5.4
 
@@ -60,6 +105,18 @@ WHERE c.orzechy IS NULL
 GROUP BY p.idpudelka
 ORDER BY 2 DESC;
 
+SELECT c.idczekoladki, COUNT(c.idczekoladki)
+FROM    pudelka p
+        JOIN zawartosc z USING(idpudelka)
+        JOIN czekoladki c USING(idczekoladki)
+GROUP BY c.idczekoladki
+ORDER BY 2;
+
+SELECT a.idpudelka, COUNT(a.idpudelka)
+FROM    zamowienia z
+        JOIN artykuly a USING(idzamowienia)
+GROUP BY a.idpudelka;
+
 -- 5.5
 
 -- SELECT COUNT(*)
@@ -67,14 +124,25 @@ ORDER BY 2 DESC;
 -- GROUP BY datarealizacji
 -- ORDER BY 2 DESC;
 
-SELECT date_part('month', datarealizacji), COUNT(*)
-FROM zamowienia
-GROUP BY date_part('month', datarealizacji)
-ORDER BY 2 DESC;
+-- SELECT date_part('month', datarealizacji), COUNT(*)
+-- FROM zamowienia
+-- GROUP BY date_part('month', datarealizacji)
+-- ORDER BY 2 DESC;
 
 -- 5.6
 
-SELECT SUM(c.masa * z.sztuk) * p.stan
+SELECT p.idpudelka, SUM(c.masa * z.sztuk) * p.stan
+FROM    pudelka p
+        JOIN zawartosc z USING(idpudelka)
+        JOIN czekoladki c USING(idczekoladki)
+GROUP BY p.idpudelka;
+
+SELECT SUM(p.cena * p.stan)
+FROM pudelka p;
+
+-- 5.7
+
+SELECT p.idpudelka, p.cena - SUM(c.koszt * z.sztuk) AS Zysk
 FROM    pudelka p
         JOIN zawartosc z USING(idpudelka)
         JOIN czekoladki c USING(idczekoladki)
